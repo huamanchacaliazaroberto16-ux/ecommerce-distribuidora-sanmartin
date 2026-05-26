cf25fc7ac195d9a2b4af821b1682660d2dd888f7
Roberto <huamanchacaliazaroberto16@gmail.com>
2026-05-26 17:32:03 -0500

    Estructura oficial Spring Boot con arquitectura MVC y capas DAO

---
+++ src/main/java/com/distribuidora/sanmartin/SanmartinApplication.java
@@ -0,0 +1,13 @@
+package com.distribuidora.sanmartin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanmartinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanmartinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/distribuidora/sanmartin/SanmartinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distribuidora.sanmartin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanmartinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
